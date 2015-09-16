8ef3918ce6f76d22b5313a0c40c9022613da2943
Jeff Kereakoglow <jkereako@alexisdigital.com>
2015-09-15 23:24:52 -0400

    Initial commit.

---
+++ TableViewBackgroundImage/TableViewBackgroundImage/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TableViewBackgroundImage
+//
+//  Created by Jeff Kereakoglow on 9/15/15.
+//  Copyright © 2015 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+}
+
---
+++ TableViewBackgroundImage/TableViewBackgroundImage/TableViewBackgroundImage.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewBackgroundImage.swift
+//  TableViewBackgroundImage
+//
+//  Created by Jeff Kereakoglow on 9/15/15.
+//  Copyright © 2015 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableTableView: UITableView {
+
+  /**
+
+  :see: http://stackoverflow.com/questions/5825397/uitableview-background-image#32215518
+  */
+  @IBInspectable var backgroundImage: UIImage? {
+    didSet {
+      if let image = backgroundImage {
+        let backgroundImage = UIImageView(image: image)
+        backgroundImage.contentMode = UIViewContentMode.ScaleToFill
+        backgroundImage.clipsToBounds = false
+        self.backgroundView = backgroundImage
+      }
+    }
+  }
+
+}
